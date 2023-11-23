1824e882cb70d801f2e5c2c5a6d48884dc5ec87f
vagnerbence <vagner.bence@gmail.com>
2023-11-23 20:07:06 +0100

    index.php adding

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+//alkalmaz�s gy�k�r k�nyvt�ra a szerveren
+define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'].'/web2-utazas/');
+
+//URL c�m az alkalmaz�s gy�ker�hez
+define('SITE_ROOT', 'http://localhost/web2-utazas/');
+
+// a router.php vez�rl� bet�lt�se
+require_once(SERVER_ROOT . 'controllers/' . 'router.php');
+
+?>
